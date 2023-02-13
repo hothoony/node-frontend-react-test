0273c462bc1d3c91638ce6cd6f70c73a9a9a1aa3
hothoony <hothoony@hanmail.net>
2023-02-13 14:43:12 +0900

    .

--- react-chartjs-2-test/src/App.js
+++ react-chartjs-2-test/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import VerticalChartV1 from './components/VerticalChartV1';
 import VerticalChartV2 from './components/VerticalChartV2';
 import MemberChartV1 from './components/MemberChartV1';
+import MemberChartV2 from './components/MemberChartV2';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <MemberChartV2/>
         <MemberChartV1/>
         <VerticalChartV1/>
         <VerticalChartV2/>
