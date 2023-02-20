5d173079da866f9f3d56311a828c900837c10a5c
hothoony <hothoony@hanmail.net>
2023-02-20 23:17:31 +0900

    .

---
+++ react-router-nested-routing-test/src/components/Login.js
@@ -0,0 +1,23 @@
+const Login = () => {
+    return (
+        <div>
+            <div>
+                <label>
+                    <span>id</span>
+                    <input type="text"/>
+                </label>
+            </div>
+            <div>
+                <label>
+                    <span>pw</span>
+                    <input type="password"/>
+                </label>
+            </div>
+            <div>
+                <button>login</button>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
