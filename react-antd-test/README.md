2e2ec866be186550e6232dffcc08530ea698e18a
hothoony <hothoony@hanmail.net>
2023-03-21 18:07:36 +0900

    .

--- react-antd-test/README.md
+++ react-antd-test/README.md
@@ -1,10 +1,14 @@
 ## 프로젝트 생성
-```
+```shell
 $ npx create-react-app react-antd-test
 ```
 
 ## antd 모듈 설치
-```
+```shell
 $ npm install antd
 $ npm install @ant-design/icons
+```
+## 추가 라이브러리 설치
+```shell
+$ npm install dayjs
 ```
--- react-antd-test/src/components/form/SearchForm.js
+++ react-antd-test/src/components/form/SearchForm.js
@@ -1,20 +1,42 @@
 import { Button, DatePicker, Select } from "antd";
 import Search from "antd/es/input/Search";
+import dayjs from "dayjs";
 import { useEffect, useState } from "react";
+
+const lpad2 = (str) => {
+    let s = String(str);
+    while (s.length < 2) {
+        s = '0' + s;
+    }
+    return s;
+}
+
+const formatDate = (date) => {
+    const yyyy = date.getFullYear();;
+    const mm = lpad2(date.getMonth() + 1);
+    const dd = lpad2(date.getDate());
+    return `${yyyy}-${mm}-${dd}`;
+}
 
 const SearchForm = () => {
 
     const [listSize, setListSize] = useState('25'); // 데이터가 string 이기 때문에 string 으로 셋팅해야 함
     const [keyword, setKeyword] = useState('');
-    const [searchOption, setSearchOption] = useState('name');
+    const [keywordType, setKeywordType] = useState('name');
     const [openYn, setOpenYn] = useState('');
+
+    const [startDate, setStartDate] = useState('');
+    // const [endDate, setEndDate] = useState('');
+    // const [endDate, setEndDate] = useState('2023-03-21');
+    const [endDate, setEndDate] = useState(formatDate(new Date()));
 
     useEffect(() => {
         doSearch();
+        return () => {}
     }, [listSize, openYn]);
     
     const onSelectChange = (e) => {
-        console.log('onSelectChange', e);
+        // console.log('onSelectChange', e);
         setListSize(e);
     }
 
@@ -24,25 +46,40 @@ const SearchForm = () => {
     }
 
     const onOpenFlChange = (e) => {
-        console.log('onOpenFlChange', e);
+        // console.log('onOpenFlChange', e);
         setOpenYn(e);
     }
 
-    const onSearchOptionChange = (e) => {
-        console.log('onSearchOptionChange');
-        setSearchOption(e);
+    const onKeywordTypeChange = (e) => {
+        // console.log('onKeywordTypeChange');
+        setKeywordType(e);
     }
 
     const onSearchButtonClick = () => {
         doSearch();
     }
 
+    const onStartDateChange = (e) => {
+        console.log('onStartDateChange', e);
+        console.log('e.$d', e.$d);
+        console.log('formatDate(e.$d)', formatDate(e.$d));
+        setStartDate(formatDate(e.$d));
+        // setStartDate('2023-03-01');
+    }
+
+    const onEndDateChange = (e) => {
+        console.log('onEndDateChange', e);
+        setEndDate(formatDate(e.$d));
+    }
+
     const doSearch = (e) => {
         const params = {
             listSize,
-            searchOption,
-            keyword,
             openYn,
+            startDate,
+            endDate,
+            keywordType,
+            keyword,
         };
         console.log('doSearch params', params);
     }
@@ -82,20 +119,30 @@ const SearchForm = () => {
 
             <div>
                 <DatePicker
-                    // value={'2023-03-03'}
+                    placeholder="시작일"
+                    // value={startDate}
+                    onChange={onStartDateChange}
+                />
+                ~
+                <DatePicker
+                    placeholder="종료일"
+                    defaultValue={dayjs(endDate, 'YYYY-MM-DD')}
+                    // defaultValue={dayjs(formatDate(new Date()), 'YYYY-MM-DD')}
+                    // value={endDate}
+                    onChange={onEndDateChange}
                 />
             </div>
 
             <div>
                 <Select
                     style={{ width : 80 }}
-                    value={searchOption}
+                    value={keywordType}
                     options={[
                         {value: 'name', label: '이름'},
                         {value: 'email', label: '이메일'},
                         {value: 'mobile', label: '핸드폰'},
                     ]}
-                    onChange={onSearchOptionChange}
+                    onChange={onKeywordTypeChange}
                 />
 
                 <Search
