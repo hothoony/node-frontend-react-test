365c3f2e89b9e892de15422f96ae3d6dbc004195
hothoony <hothoony@hanmail.net>
2024-09-11 13:25:52 +0900

    .

--- react-form-test2/src/pages/Menu.tsx
+++ react-form-test2/src/pages/Menu.tsx
@@ -7,7 +7,7 @@ export default function Menu() {
                 <Link to={'/formTemplate'}>FormTemplate</Link>
             </div>
             <div>
-                <h2>form 을 사용하는 여러가지 방법</h2>
+                <h2>react 에서 form 사용하는 방법</h2>
                 <ul>
                     <li>
                         <Link to={'/form01'}>1. useState 와 handler 를 사용</Link>
