c18d7e9d710c1b6326cb53f300658d6eff2bdc42
hothoony <hothoony@hanmail.net>
2024-09-11 09:36:06 +0900

    .

--- react-form-test2/src/pages/Menu.tsx
+++ react-form-test2/src/pages/Menu.tsx
@@ -3,28 +3,24 @@ import { Link } from "react-router-dom";
 export default function Menu() {
     return (
         <div>
-
-            <div>
-                form test
-                <ul>
-                    <li>
-                        <Link to={'/form01'}>1. state 와 handler 를 사용</Link>
-                    </li>
-                    <li>
-                        <Link to={'/form02'}>2. Ref 를 사용</Link>
-                    </li>
-                    <li>
-                        <Link to={'/form03'}>3. FormData 와 브라우저 API 사용</Link>
-                    </li>
-                    <li>
-                        <Link to={'/form04'}>4. custom hook 을 사용</Link>
-                    </li>
-                    <li>
-                        <Link to={'/form05'}>5. React Forms Library 를 사용</Link>
-                    </li>
-                </ul>
-            </div>
-
+            <h2>form 을 사용하는 여러가지 방법</h2>
+            <ul>
+                <li>
+                    <Link to={'/form01'}>1. state 와 handler 를 사용</Link>
+                </li>
+                <li>
+                    <Link to={'/form02'}>2. Ref 를 사용</Link>
+                </li>
+                <li>
+                    <Link to={'/form03'}>3. FormData 와 브라우저 API 사용</Link>
+                </li>
+                <li>
+                    <Link to={'/form04'}>4. custom hook 을 사용</Link>
+                </li>
+                <li>
+                    <Link to={'/form05'}>5. React Forms Library 를 사용</Link>
+                </li>
+            </ul>
         </div>
     );
 }
